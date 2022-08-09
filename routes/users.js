ae4b70197997f220dac3b9c8c2abcbaf84bb6886
Rachelidekel <dudinka77@gmail.com>
2022-08-09 09:45:05 +0300

    new update for my project

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
-const router = require("express").Router();
-const { getUsers, getUserId } = require("../controllers/users");
+const router = require('express').Router();
+const { getUsers, getUserId } = require('../controllers/users');
 
-router.get("/", getUsers);
+router.get('/', getUsers);
 
-router.get("/:user_id", getUserId);
+router.get('/:user_id', getUserId);
 
 module.exports = router;
