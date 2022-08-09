136da398de6faf359d3ba711d939fe382051eee5
Rachelidekel <dudinka77@gmail.com>
2022-08-09 11:56:48 +0300

    new update for my project

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const helmet = require('helmet');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const indexRouter = require('./routes/indes');
 
 const { PORT = 3000 } = process.env;
 
@@ -11,11 +12,6 @@ app.use(helmet());
 
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+app.use('*', indexRouter);
 
-app.get('*', (req, res) => {
-  res.status(404).send({ message: 'Requested resource not found' });
-});
-
-app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}`);
-});
+app.listen(PORT);
