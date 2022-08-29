7ca16afd39a59b754d3533014ad19ba7e0fcdd39
Rachelidekel <dudinka77@gmail.com>
2022-08-29 18:48:36 +0300

    new update for my project

--- app.js
+++ app.js
@@ -12,12 +12,7 @@ const { PORT = 3000 } = process.env;
 
 app.use(helmet());
 
-mongoose.connect('mongodb://localhost:27017/aroundb',{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  });
-
+mongoose.connect('mongodb://localhost:27017/aroundb')
 
 app.use((req, res, next) => {
 req.user = {_id:'5d8b8592978f8bd833ca8133'};
